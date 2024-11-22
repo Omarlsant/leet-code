44fa978d7db51c3bd470deccc20df9fac7d13936
Omar Lengua <omarns21@gmail.com>
2024-11-22 15:47:51 +0100

    Changes in the file

---
+++ intro-to-pandas/9-change-data-type.py
@@ -0,0 +1,6 @@
+import pandas as pd  # Importing the pandas library for data manipulation.
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    # Converts the 'grade' column in the DataFrame to integer type.
+    students['grade'] = students['grade'].astype(int)
+    return students  # Returns the modified DataFrame.
